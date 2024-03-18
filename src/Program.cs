0d7799523a2c16d4b1672606a512c596747e1275
Shama Alkhaldi <Shama@alMacBook-Air-alkhas-bbdalzyz.local>
2024-03-18 14:17:15 +0300

    Finished all requirement

--- src/Program.cs
+++ src/Program.cs
@@ -1,22 +1,63 @@
-﻿string TempConvert(float temperature, string unit){
-        switch (unit)
-        {
-            case "C":
-                float cel = temperature * 9 / 5 + 32;
-                return $"{Math.Round((decimal)cel, 2)} F";
-            case "F":
-                float fr = (temperature - 32) * 5 / 9;
-                return $"{Math.Round((decimal)fr, 2)} C";
-            default:
-                break;
-        }
-        return "Enter correct unit";
+﻿string TempConvert(float temperature, char unit)
+{
+    switch (unit)
+    {
+        case 'c':
+            float cel = temperature * 9 / 5 + 32;
+            return $"{Math.Round((decimal)cel, 2)} F";
+        case 'f':
+            float fr = (temperature - 32) * 5 / 9;
+            return $"{Math.Round((decimal)fr, 2)} C";
+        default:
+            break;
     }
+    return "Invalid scale. Please enter 'C' for Celsius or 'F' for Fahrenheit.";
+}
 
-Console.WriteLine("Enter a temperature:");
-string? temperature = Console.ReadLine();
+Boolean isContinue = true;
 
-Console.WriteLine("Enter a unit (C or F):");
-string? unit = Console.ReadLine();
+while (isContinue)
+{
 
-Console.WriteLine(TempConvert(float.Parse(temperature), unit.ToUpper()));
+    try
+    {
+        Console.WriteLine("Enter a temperature:");
+        float temperature = Convert.ToInt64(Console.ReadLine());
+
+        Console.WriteLine("Enter a unit (C or F):");
+        char unit = Char.ToLower(Convert.ToChar(Console.ReadLine()));
+        if (unit == 'f' || unit == 'c')
+        {
+            Console.WriteLine(TempConvert(temperature, unit));
+        }
+        else
+        {
+
+            throw new ArgumentException();
+        }
+
+        Console.WriteLine("Do you went to Continue (y/n):");
+        // string input = ;
+        if (Console.ReadLine().ToLower() == "n")
+        {
+            isContinue = false;
+        }
+
+
+
+    }
+    catch (System.FormatException)
+    {
+        Console.WriteLine("Invalid input. Please enter a numeric temperature.");
+    }
+    catch (ArgumentException e)
+    {
+        Console.WriteLine("Invalid scale. Please enter 'C' for Celsius or 'F' for Fahrenheit.");
+    }
+    catch (System.Exception)
+    {
+        throw;
+    }
+}
+
+Console.WriteLine("Program terminated");
