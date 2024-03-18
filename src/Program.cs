fd814c49da5be4f6732136813e8096a1e63df471
Shama Alkhaldi <Shama@alMacBook-Air-alkhas-bbdalzyz.local>
2024-03-18 12:44:25 +0300

    prompt the user for a temperature and unit

--- src/Program.cs
+++ src/Program.cs
@@ -1,14 +1,22 @@
-﻿static string TempConvert(int temperature, char unit){
-        if (unit == 'C'){
-            int fr = temperature * 9 / 5 + 32;
-            return fr + " F";
+﻿static string TempConvert(float temperature, string unit){
+        float tempResult = 0;
+        string tempUnitResult = "";
+
+        if (unit == "C"){
+            tempResult = temperature * 9 / 5 + 32;
+            tempUnitResult = "F";
         }
-        else if (unit == 'F'){
-            int cel = (temperature - 32) * 5 / 9;
-            return cel+ " C";
+        else if (unit == "F"){
+            tempResult = (temperature - 32) * 5 / 9;
+            tempUnitResult = "C";
         }
-        return "Enter correct measurement unit";
+        return $"{Math.Round((decimal)tempResult, 2)} {tempUnitResult}";
     }
 
-Console.WriteLine(TempConvert(32, 'F'));
-Console.WriteLine(TempConvert(100, 'C'));
+Console.WriteLine("Enter a temperature:");
+string? temperature = Console.ReadLine();
+
+Console.WriteLine("Enter a unit (C or F):");
+string? unit = Console.ReadLine();
+
+Console.WriteLine(TempConvert(float.Parse(temperature), unit.ToUpper()));
