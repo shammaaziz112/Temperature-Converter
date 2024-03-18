cf0c9cfc16606007caf880127211b836ae863c09
Shama Alkhaldi <Shama@alMacBook-Air-alkhas-bbdalzyz.local>
2024-03-18 11:42:03 +0300

    Implements a TempConvert function

--- src/Program.cs
+++ src/Program.cs
@@ -1,1 +1,13 @@
-﻿Console.WriteLine("Hello, World!");
+﻿static void TempConvert(int temperature, char unit){
+        if (unit == 'C'){
+            int fr = temperature * 9 / 5 + 32;
+            Console.WriteLine(fr + " F");
+        }
+        else if (unit == 'F'){
+            int cel = (temperature - 32) * 5 / 9;
+            Console.WriteLine(cel+ " C");
+        }
+    }
+
+TempConvert(32, 'F');
+TempConvert(100, 'C');
