4ac89cc8bb64066190ff6014d1a30eb5a85fdb6b
Shama Alkhaldi <Shama@alMacBook-Air-alkhas-bbdalzyz.local>
2024-03-18 12:51:27 +0300

    edit function

--- src/Program.cs
+++ src/Program.cs
@@ -1,16 +1,16 @@
-﻿static string TempConvert(float temperature, string unit){
-        float tempResult = 0;
-        string tempUnitResult = "";
-
-        if (unit == "C"){
-            tempResult = temperature * 9 / 5 + 32;
-            tempUnitResult = "F";
+﻿string TempConvert(float temperature, string unit){
+        switch (unit)
+        {
+            case "C":
+                float cel = temperature * 9 / 5 + 32;
+                return $"{Math.Round((decimal)cel, 2)} F";
+            case "F":
+                float fr = (temperature - 32) * 5 / 9;
+                return $"{Math.Round((decimal)fr, 2)} C";
+            default:
+                break;
         }
-        else if (unit == "F"){
-            tempResult = (temperature - 32) * 5 / 9;
-            tempUnitResult = "C";
-        }
-        return $"{Math.Round((decimal)tempResult, 2)} {tempUnitResult}";
+        return "Enter correct unit";
     }
 
 Console.WriteLine("Enter a temperature:");
